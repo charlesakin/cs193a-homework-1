935f50350aec0f089772809508181f3ad038c489
Charles Akin-David <aakindav@stanford.edu>
2016-01-14 23:04:43 -0800

    Added outcome message and README

--- app/src/main/java/com/example/charlesakin_david/homework1/RPS.java
+++ app/src/main/java/com/example/charlesakin_david/homework1/RPS.java
@@ -10,6 +10,7 @@ import java.util.Random;
 public class RPS extends AppCompatActivity {
 
     private int points = 0;
+    private Random ran = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,25 +27,52 @@ public class RPS extends AppCompatActivity {
     */
     public void choice(View view) {
         int id = view.getId();
-        Random ran = new Random();
         int comp = ran.nextInt(3);
         ImageView imgComp = (ImageView) findViewById(R.id.computer);
         if (comp == 0) imgComp.setImageResource(R.drawable.lrock);
         if (comp == 1) imgComp.setImageResource(R.drawable.lpaper);
         if (comp == 2) imgComp.setImageResource(R.drawable.lscissors);
         ImageView img = (ImageView) findViewById(R.id.human);
+        TextView resultView = (TextView) findViewById(R.id.result);
         if (id == R.id.rock) {
             img.setImageResource(R.drawable.rock);
-            if (comp == 1) points--;
-            if (comp == 2) points++;
+            if (comp == 1) {
+                points--;
+                resultView.setText("You Lose! :(");
+            }
+            else if (comp == 2) {
+                points++;
+                resultView.setText("You Win! :D");
+            }
+            else {
+                resultView.setText("Its a tie! :/");
+            }
         } else if (id == R.id.paper) {
             img.setImageResource(R.drawable.paper);
-            if (comp == 2) points--;
-            if (comp == 0) points++;
+            if (comp == 2) {
+                points--;
+                resultView.setText("You Lose! :(");
+            }
+            else if (comp == 0) {
+                points++;
+                resultView.setText("You Win! :D");
+            }
+            else{
+                resultView.setText("Its a tie! :/");
+            }
         } else {
             img.setImageResource(R.drawable.scissors);
-            if (comp == 0) points--;
-            if (comp == 1) points++;
+            if (comp == 0) {
+                points--;
+                resultView.setText("You Lose! :(");
+            }
+            else if (comp == 1) {
+                points++;
+                resultView.setText("You Win! :D");
+            }
+            else {
+                resultView.setText("Its a tie! :/");
+            }
         }
 
         TextView pointView = (TextView) findViewById(R.id.points);
---
+++ app/src/main/res/README.txt
@@ -0,0 +1,7 @@
+Charles AKin-David <aakindav@stanford.edu>
+Rock, Paper, Scissors 0.8 - This app allows the user to play the game
+of Rock, Paper, Scissors against a computer. The user clicks the radio
+button represent their choice of hand throw. The computer will then
+randomly choice a hand throw. The user will receive a point if they beat
+the computer (Rock > Scissors, Scissors > Paper, Paper > Rock) and will
+lose a point if they lose to the computer. No points are assigned for ties.
